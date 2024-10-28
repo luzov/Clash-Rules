566b73225ee77f3374f967262e6867660a8b48e3
yangtb2024 <164613316+yangtb2024@users.noreply.github.com>
2024-10-28 15:29:19 +0800

    Update 杀死李志坚.js

--- 杀死李志坚.js
+++ 杀死李志坚.js
@@ -6,27 +6,13 @@ function main(params) {
 function addRejectIPRuleWithHighestPriority(params, ipAddress) {
 	const lanRuleIndex = params.rules.findIndex(rule => rule.startsWith("GEOIP,LAN,DIRECT"));
 	
-	if (!params["rule-providers"]) {
-		params["rule-providers"] = {};
-	}
-	
-	const ruleSetName = "block_udp_traffic";
-	
-	params["rule-providers"][ruleSetName] = {
-		"type": "http",
-		"url": "https://raw.githubusercontent.com/yangtb2024/Steam-Clash/refs/heads/main/block_udp_traffic.yaml",
-		"behavior": "classical"
-	};
-	
 	if (lanRuleIndex !== -1) {
 		params.rules.splice(lanRuleIndex + 1, 0, `IP-CIDR,${ipAddress}/32,REJECT`);
 		params.rules.splice(lanRuleIndex + 2, 0, `PROCESS-NAME,ywSMPAgent.exe,REJECT`);
 		params.rules.splice(lanRuleIndex + 3, 0, `PROCESS-NAME,ywSMPASvr.exe,REJECT`);
-		params.rules.splice(lanRuleIndex + 4, 0, `RULE-SET,${ruleSetName},REJECT`); 
 	} else {
 		params.rules.unshift(`IP-CIDR,${ipAddress}/32,REJECT`);
 		params.rules.unshift(`PROCESS-NAME,ywSMPAgent.exe,REJECT`);
 		params.rules.unshift(`PROCESS-NAME,ywSMPASvr.exe,REJECT`);
-		params.rules.unshift(`RULE-SET,${ruleSetName},REJECT`); 
 	}
 }
